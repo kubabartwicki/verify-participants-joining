ee1880d9f89718d91a44042853374bb75c47ce54
Kuba Bartwicki <kuba.bartwicki@digital.cabinet-office.gov.uk>
2019-12-04 17:09:32 +0000

    New architecture thing

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,27 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+function renderPages(companyName, links) {
+	router.get('/' + companyName, function (req, res) {
+	  res.render('templates/index', links);
+	});
+	router.get('/' + companyName + '/*', function (req, res) {
+	  var path = req.path
+	  var cleared = path.replace('/' + companyName + '/','')
+	  res.render('templates/' + cleared, links);
+	});
+}
+
+// demo
+renderPages('drxik', { 
+	'url' : 'drxik',
+	'variable' : 'value A' 
+}) 
+
+// sitekit
+renderPages('kzhnz', { 
+	'url' : 'kzhnz',
+	'variable' : 'value B' 
+}) 
+
 module.exports = router
