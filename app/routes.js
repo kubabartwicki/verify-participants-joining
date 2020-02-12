0c2db6d24d6d4902cedb3bba5c2f3ab497a01e07
Kuba Bartwicki <kuba.bartwicki@digital.cabinet-office.gov.uk>
2020-02-12 15:40:28 +0000

    Remove logic since we no longer need it

--- app/routes.js
+++ app/routes.js
@@ -1,35 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-
-function renderPages(companyName, links) {
-	router.get('/' + companyName, function (req, res) {
-	  res.render('templates/index', links);
-	});
-	router.get('/' + companyName + '/*', function (req, res) {
+function redirect() {
+	router.get('/drxik/*', function (req, res) {
 	  var path = req.path
-	  var cleared = path.replace('/' + companyName + '/','')
-	  res.render('templates/' + cleared, links);
+	  var cleared = path.replace('/drxik/','/')
+	  res.redirect(cleared);
 	});
 }
-
-// demo 
-renderPages('drxik', { 
-	'url' : 'drxik',
-	'beingIdentityProvider' : 'https://paper.dropbox.com/doc/Identity-providers-on-the-trust-framework-what-you-must-do-C9LFORoDTa2aQLIByOj0D',
-	'usingCredentials' : 'https://paper.dropbox.com/doc/Using-credentials-to-protect-an-online-service--Ap7Ckf36cQzXIHkiNSKEfIBpAQ-ysh0jYSD54HULYfFwyQ1P',
-	'rolesAndResponsibilities' : '#',
-	'introduction' : '#'
-})
-
-// condatis
-renderPages('kzhnz', { 
-	'url' : 'kzhnz',
-	'beingIdentityProvider' : 'https://blog.gemalto.com/wp-content/uploads/2018/02/digital-id.jpg',
-	'usingCredentials' : 'https://paper.dropbox.com/doc/Using-credentials-to-protect-an-online-service--Ap7Ckf36cQzXIHkiNSKEfIBpAQ-ysh0jYSD54HULYfFwyQ1P',
-	'rolesAndResponsibilities' : '#',
-	'introduction' : '#'
-}) 
+redirect()
 
 module.exports = router
+
+// if someone is on 'drxik/' + path
+// take em back to path
